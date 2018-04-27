2aba5b77845812a99fcd9cf0fd4cb784303a599d
Nguyen Thiet Lap <lapnguyen.se@gmail.com>
2018-04-27 21:00:16 +1000

    add new image

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,7 +7,7 @@ import {ShoppingListService} from '../shopping-list/shopping-list.service';
 export class RecipeService {
   private loremString = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
   private recipes: Recipe[] = [
-    new Recipe('Hambuger', this.loremString, 'http://stormp.vn/wp-content/uploads/2015/07/08-Bacon-Cheese-Burger-960x576.jpg', [new Ingredient('Cheese', 1), new Ingredient('Miced Beef', 500), new Ingredient('Hambuger Bun', 1), new Ingredient('Salad', 100)]),
+    new Recipe('Hambuger', this.loremString, 'https://images.unsplash.com/photo-1468071174046-657d9d351a40?ixlib=rb-0.3.5&s=ba7023d8300e46c64ac7e6793a21a6bd&auto=format&fit=crop&w=750&q=60', [new Ingredient('Cheese', 1), new Ingredient('Miced Beef', 500), new Ingredient('Hambuger Bun', 1), new Ingredient('Salad', 100)]),
     new Recipe('Clubhouse Sandwich', this.loremString, 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--653594_11.jpg?itok=9trM5bYp', [new Ingredient('White bread Slices', 3), new Ingredient('Lettuce', 2), new Ingredient('Cheese slice', 1), new Ingredient('Tomato', 1)])
   ];
 
